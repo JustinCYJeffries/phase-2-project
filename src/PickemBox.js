ea72ec7015f57ed24e0eabd02dbeccb6f98df3ef
Justin Jeffries <justincyjeffries@gmail.com>
2022-05-25 21:57:51 -0600

    renders from elements

--- src/PickemBox.js
+++ src/PickemBox.js
@@ -4,31 +4,35 @@ function PickemBox({teams, teamData, setTeamData}){
 
     const homeTeam=teams[1].id
     const awayTeam = teams[0].id
-    
+   let awayElement = [] 
+   let homeElement = [] 
 
     
     const foundHomeTeam = teamData.map(element => {
         if (element.team.abbreviation == homeTeam)
+        homeElement = element.team
         return(
             element.team.displayName
         )
     });
     
     const foundAwayTeam = teamData.map(element => {
-        if (element.team.abbreviation == awayTeam)
-        return(
+        if (element.team.abbreviation == awayTeam){
+            awayElement = element.team
+            return(
             element.team.displayName
-        )
+            
+        )}
     });
-
+    
 
 
 
     return(
         <div className='box'>
-            <h3 className='text_center'>{foundAwayTeam} @ {foundHomeTeam}</h3>
-            <button>{teams[0].id} Win</button>
-            <button>{teams[1].id} Win</button>
+            <h3 className='text_center'>{awayElement.displayName} @ {homeElement.displayName}</h3>
+            <button>{awayElement.shortDisplayName} Win</button>
+            <button>{homeElement.shortDisplayName} Win</button>
         </div>
     )
 }
