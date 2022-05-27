2f1a1af6808ad494f4c6840a6c3bc9fe89f630e4
Justin Jeffries <justincyjeffries@gmail.com>
2022-05-26 20:13:50 -0600

    replaces buttons with winners

--- src/Buttons.js
+++ src/Buttons.js
@@ -4,20 +4,30 @@ import React, {useState} from "react"
 
 
 function Buttons({homeElement, awayElement, teamData, handleClick, winTeam}) {
- let pickedWinner=[]
+  
+  
   const [picked, setPicked] = useState(false)
+  const [winName, setWinName] = useState("")
+
+  function partyClick(e){
+   setPicked(true)
+   handleClick(e)
+   setWinName(e.target.attributes.winnamez.value)
+ }
+
  
   function picking(){
     if(picked==false){
       return(
         <div>
-      <button onClick={e=>handleClick(e)} value={awayElement.abbreviation} winner={awayElement.abbreviation} loser={homeElement.abbreviation}>{awayElement.shortDisplayName} Win</button>
-      <button onClick={e=>handleClick(e)}value={homeElement.abbreviation} winner={homeElement.abbreviation} loser={awayElement.abbreviation}>{homeElement.shortDisplayName} Win</button>
+      <button onClick={e=>partyClick(e)} value={awayElement.abbreviation} winner={awayElement.abbreviation} loser={homeElement.abbreviation} winnamez={awayElement.shortDisplayName}>{awayElement.shortDisplayName} Win</button>
+      <button onClick={e=>partyClick(e)}value={homeElement.abbreviation} winner={homeElement.abbreviation} loser={awayElement.abbreviation}  winnamez={homeElement.shortDisplayName}>{homeElement.shortDisplayName} Win</button>
     </div>
       )
     }
-    else return(setPicked(true))
-
+    else return(<div>
+      <h3>{winName} WINS!!</h3>
+    </div>)
   }
    
 
